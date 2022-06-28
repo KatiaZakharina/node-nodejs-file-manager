c8d807ee8e7dee9e662b99f1eb781254e512f22c
Katia <katia.zakharina@gmail.com>
2022-06-28 12:41:40 +0300

    refactor: change executor structure

--- helpers/executor.service.js
+++ helpers/executor.service.js
@@ -1,31 +1,30 @@
-import description from '../commands/description.js';
 import logService from '../service/log.service.js';
 
 class Executor {
-  commander = new Map();
-  constructor() {
-    this.commander.set(['up', 'cd', 'ls'], () => console.log('navigation'));
-    this.commander.set(['cat', 'add', 'rn', 'cp', 'mv', 'rm'], () =>
-      console.log('fs')
-    );
-    this.commander.set(['os'], () => console.log('os'));
-    this.commander.set(['hash'], () => console.log('hash'));
-    this.commander.set(['compress', 'decompress'], () => console.log('zip'));
-  }
+  commander = {
+    help: help,
+    up: up,
+    cd: cd,
+    ls: list,
+    cat: readFile,
+    add: addFile,
+    rn: renameFile,
+    cp: copyFile,
+    mv: moveFile,
+    rm: deleteFile,
+    os: os,
+    hash: hash,
+    compress: compress,
+    decompress: decompress,
+    default: () => logService.printError(`Invalid input! Try again :)`),
+  };
 
   execute(commandList) {
     const [command, ...parameters] = commandList;
-    console.log(command);
 
-    if (!Object.keys(description).includes(command)) {
-      logService.printError('Invalid input');
-    }
-
-    for (const [commands, executor] of this.commander) {
-      if (commands.includes(command)) {
-        executor(parameters);
-      }
-    }
+    command in this.commander
+      ? this.commander[command](parameters)
+      : this.commander.default();
   }
 }
 
